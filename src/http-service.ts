10e1f9c3cdeb33f8b58954c41de4bb6bb3574c03
Ryan Mackey-Paulsen <ryan.paulsen@cookmedical.com>
2016-09-05 13:29:11 -0400

    created base service and decorator

--- src/http-service.ts
+++ src/http-service.ts
@@ -1,4 +1,64 @@
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs';
 
-export function HttpRequest() {}
+
+export abstract class HttpBaseService {
+	protected cache: any[] = [];
+	protected requests: { [key: string]: Observable<any> } = {};
+
+	public getCacheKey(fName: string, ...args: any[]): string {
+		let argsString = args.join('_');
+		return `${fName}:${argsString}`;
+	}
+
+	public clearCache(key) {
+		if ( key ) {
+			this.cache = this.cache.filter(item => item.id !== key);
+		} else {
+			this.cache = [];
+		}
+	}
+}
+
+export function HttpRequest(options?: {cache?: boolean}) {
+	if ( !options ) {
+		options = {
+			cache: false
+		}
+	}
+
+	return function(target, key, descriptor) {
+		let originalFunction = descriptor.value;
+
+		descriptor.value = function(...args: any[]) {
+			let argsString = args.join('_');
+			let requestKey = `${key}:${argsString}`;
+
+			this.cache = this.cache || [];
+			this.requests = this.requests || {};
+
+			let cached = this.cache.filter(item => item.id === requestKey).pop();
+			if ( cached ) {
+				return Observable.from([cached.data]);
+			}
+
+			if ( !this.requests[requestKey] ) {
+				this.requests[requestKey] = originalFunction.bind(this).call(args).publish().refCount();
+			}
+
+			this.requests[requestKey].subscribe((res)=>{
+				if ( options.cache ) {
+					this.cache.push({
+						id: requestKey,
+						data: res
+					});
+				}
+				this.requests[requestKey] = null;
+			});
+
+			return this.requests[requestKey];
+		};
+
+		return descriptor;
+	}
+}
